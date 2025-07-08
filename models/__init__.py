9c6e896b740fc64a7ef8589a542de01b05a3d3b7
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-07-08 15:34:11 -0600

    added .gitignore

---
+++ models/__init__.py
@@ -0,0 +1,16 @@
+#from .cifar10STOCH_quantizedoctav import *
+#from .cifar10DET_quantizedoctav import *
+#from .cifar10STOCH_quantizedmaxscalar import *
+#from .cifar10STOCH_quantizedmaxscalar import *
+#from .cifar10_FP import *
+#
+#from .mnistSTOCH_quantizedoctav import *
+#from .mnistDET_quantizedoctav import *
+#from .mnistSTOCH_quantizedmaxscalar import *
+#from .mnistSTOCH_quantizedmaxscalar import *
+from .mnist_FP import *
+
+from .ComplexResNet import *
+from .RealResNet import * 
+
+
