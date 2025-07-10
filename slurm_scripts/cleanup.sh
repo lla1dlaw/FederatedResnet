7a06af4354db05a1afeacc8621403cfa85c33803
Liam Laidlaw <liamlaidlaw04@gmail.com>
2025-07-10 15:25:20 -0600

    fixed cleanup

--- slurm_scripts/cleanup.sh
+++ slurm_scripts/cleanup.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-echo "Looking for .out files to delete..."
 
-# First, list the files that will be deleted
-echo "The following files will be permanently deleted:"
 ls -l *.out
-
-# Ask for user confirmation before deleting
-read -p "Are you sure you want to delete these files? (y/n) " -n 1 -r
-echo # move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  echo "Deleting files..."
-  rm *.out
-  echo "Done."
-else
-  echo "Deletion cancelled."
-fi
+echo ""
+echo "Deleting files..."
+echo ""
+rm *.out
